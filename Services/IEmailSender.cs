de3c1208b436dcdd960f9cd2a847a6cac55c7d8e
İremsu Kardaş <kardasiremsu@gmail.com>
2024-03-15 23:56:10 +0300

    Encryption is added for user password and homepage view is modified.

---
+++ Services/IEmailSender.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace BitirmeProj.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string htmlMessage);
+    }
+
+    public class EmailSender : IEmailSender
+    {
+        private readonly string _smtpServer;
+        private readonly int _smtpPort;
+        private readonly string _smtpUsername;
+        private readonly string _smtpPassword;
+
+        public EmailSender(string smtpServer, int smtpPort, string smtpUsername, string smtpPassword)
+        {
+            _smtpServer = smtpServer;
+            _smtpPort = smtpPort;
+            _smtpUsername = smtpUsername;
+            _smtpPassword = smtpPassword;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            using (var client = new SmtpClient(_smtpServer, _smtpPort))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(_smtpUsername, _smtpPassword);
+                client.EnableSsl = true;
+
+                var message = new MailMessage();
+                message.To.Add(email);
+                message.Subject = subject;
+                message.Body = htmlMessage;
+                message.IsBodyHtml = true;
+
+                await client.SendMailAsync(message);
+            }
+        }
+    }
+}
